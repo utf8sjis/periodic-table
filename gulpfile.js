82071a07b19eb61aab1171dd978037e2f2759976
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2021-10-17 06:31:31 +0900

    chore: Separate development location from dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 
 
-// scssファイルの変更を監視しcssを生成する
-const compileScss = () =>
+// scssファイルの変更を監視しcssファイルを生成する
+const compileSass = () =>
   gulp.watch('app/static/scss/*.scss', () =>
     gulp.src('app/static/scss/style.scss')
       .pipe(sourcemaps.init())
@@ -18,12 +19,24 @@ const compileScss = () =>
         browsers: ['last 2 versions'],
       }))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest('dist/static/css/'))
-      .pipe(cleanCSS())
-      .pipe(rename({
-        suffix: '.min',
-      }))
-      .pipe(gulp.dest('dist/static/css/')));
+      .pipe(gulp.dest('app/static/css/')));
+
+
+// cssファイルを圧縮する
+const minifyCss = () =>
+  gulp.src('app/static/css/style.css')
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest('dist/static/css/'));
+
+
+// htmlファイル内のパスを置換
+const replacePath = () =>
+  gulp.src('app/*.html')
+    .pipe(replace('static/css/style.css', 'static/css/style.min.css'))
+    .pipe(gulp.dest('dist/'));
 
 
 // ファイルのdistへのコピー
@@ -31,11 +44,13 @@ const copyToDist = () =>
   gulp.src([
     'app/**',
     '!app/static/scss/**',
+    '!app/static/css/**',
+    '!app/*.html',
   ], {
     base: 'app/',
   })
     .pipe(gulp.dest('dist/'));
 
 
-exports.default = compileScss;
-exports.copy = copyToDist;
+exports.default = compileSass;
+exports.build = gulp.series(minifyCss, replacePath, copyToDist);
