de764daf48c8ad54834293631718cacf4ac50e8c
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2021-12-22 13:56:27 +0900

    update: Change alt attribute of images

--- app/static/js/data/nav_link_list.js
+++ app/static/js/data/nav_link_list.js
@@ -5,16 +5,15 @@ export const navLinkSectionList = [
       {
         href: 'https://twitter.com/gensokanji_bot',
         src: 'static/img/link/gensokanji_bot.png',
-        alt: 'Twitterアカウントの画像',
+        alt: 'Twitterアカウントのプロフィールの画像',
         title: '元素の漢字botさん (@gensokanji_bot) / Twitter',
         desc: '中の人運営の元素の漢字をつぶやくTwitterのbotです。',
       },
       {
         href: 'https://quiz-maker.site/quiz/play/dBxL2220181106152109',
         src: 'static/img/link/gensokanji_quiz.png',
-        alt: '',
-        title:
-          '元素の漢字クイズ | クイズメーカー - こたえてあそぶ・つくってあそぶ・クイズのプラットフォームサービス',
+        alt: '元素の漢字クイズのヘッダー画像',
+        title: '元素の漢字クイズ | クイズメーカー - こたえてあそぶ・つくってあそぶ・クイズのプラットフォームサービス',
         desc: '元素の漢字に関するクイズで特訓の成果を試せます。',
       },
     ],
