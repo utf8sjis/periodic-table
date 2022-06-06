0462bc21911c6cec66744008ff19d63147794a02
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-06-07 07:18:33 +0900

    update: Add head content

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,96 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'periodic-table',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    title: '元素の漢字周期表 - gensokanji',
+    htmlAttrs: { lang: 'ja', prefix: 'og: http://ogp.me/ns#' },
+    bodyAttrs: { class: 'body-preload' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。中国語圏では、化学元素の命名は漢字1字でなされ、部首で気体か金属かなどの性質を表します。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
+      },
+      // OGP
+      {
+        property: 'og:url',
+        content: 'https://gensokanji.netlify.app/periodic-table',
+      },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:title', content: '元素の漢字周期表 - gensokanji' },
+      {
+        property: 'og:description',
+        content:
+          '元素の漢字周期表は、中国語での元素名を並べた漢字の元素周期表です。そんな漢字だけでできた周期表を当サイトでお楽しみください。',
+      },
+      {
+        property: 'og:image',
+        content:
+          'https://gensokanji.netlify.app/periodic-table/static/img/periodic_table.png',
+      },
+      { property: 'og:site_name', content: 'gensokanji' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@gensokanji_bot' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'canonical',
+        href: 'https://gensokanji.netlify.app/periodic-table',
+      },
+      // micron.js
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://unpkg.com/webkul-micron@1.1.6/dist/css/micron.min.css',
+      },
+      // google fonts
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: true,
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Blinker:wght@200;300&family=M+PLUS+1p:wght@300;500&display=swap',
+      },
+      // material icons
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+      },
+      // fontawesome
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.11.0/css/all.css',
+      },
+    ],
+    script: [
+      // twitter widgets
+      {
+        async: true,
+        charset: 'utf-8',
+        src: 'https://platform.twitter.com/widgets.js',
+      },
+      // micron.js
+      {
+        type: 'text/javascript',
+        src: 'https://unpkg.com/webkul-micron@1.1.6/dist/script/micron.min.js',
+      },
+      // ionicons
+      {
+        type: 'module',
+        src: 'https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.esm.js',
+      },
+      {
+        nomodule: true,
+        src: 'https://unpkg.com/ionicons@4.5.10-0/dist/ionicons/ionicons.js',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
