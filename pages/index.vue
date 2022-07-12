94542ac5fac62cce0420c3594a28f035a50e3c13
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-07-12 10:34:36 +0900

    Revert "Hotfix/v4.0.1"

--- pages/index.vue
+++ pages/index.vue
@@ -20,11 +20,28 @@
 </template>
 
 <script>
+if (process.client) {
+  window.onload = () => {
+    const body = document.querySelector('body')
+    body.classList.remove('body-preload')
+  }
+}
+
 export default {
   name: 'IndexPage',
 
   head: {
     bodyAttrs: { class: 'body-preload' },
+  },
+
+  mounted() {
+    // localStorageの初期化
+    let itemStorage = JSON.parse(localStorage.getItem('itemStorage'))
+    itemStorage = {
+      themeId: itemStorage?.themeId ?? 'default',
+      periodicTableScale: itemStorage?.periodicTableScale ?? 1,
+    }
+    localStorage.setItem('itemStorage', JSON.stringify(itemStorage))
   },
 }
 </script>
